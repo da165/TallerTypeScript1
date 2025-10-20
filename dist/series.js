48ad94d9dada17e30d60e06b1ab6c059261fb253
David Ortiz <da.ortiza1@uniandes.edu.co>
2025-10-20 07:43:54 -0500

    commit final

---
+++ dist/series.js
@@ -0,0 +1,13 @@
+var series = /** @class */ (function () {
+    function serie(id, name, channel, seasons, description, link, image) {
+        this.id = id;
+        this.name = name;
+        this.channel = channel;
+        this.seasons = seasons;
+        this.description = description;
+        this.link = link;
+        this.image = image;
+    }
+    return serie;
+}());
+export { series};
